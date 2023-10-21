a430de2df13da0fff1eb7d72ab4a2576bead8068
ALMEIRA\Alexandru Manole <alexmanole87@gmail.com>
2023-10-21 20:35:04 +0300

    1 error

--- main/app.js
+++ main/app.js
@@ -1,6 +1,48 @@
 const compress = (a, b = true) => {
-	//TODO: implementați funcția
-	// TODO: implement the function
+    if (typeof a !== 'string' && !(a instanceof String)) {
+        throw new Error('InvalidType');
+    }
+
+    if (compress) {
+        let result = '';
+        let count = 1;
+
+        for (let i = 0; i < a.length; i++) {
+            if (a[i] === a[i + 1]) {
+                count++;
+            } else {
+                result += a[i] + count;
+                count = 1;
+            }
+        }
+
+        return result;
+    } else {
+        let result = '';
+        let i = 0;
+
+        while (i < a.length) {
+            const char = a[i];
+            i++;
+            let count = '';
+
+            while (i < a.length && !isNaN(parseInt(a[i], 10))) {
+                count += a[i];
+                i++;
+            }
+
+            count = parseInt(count, 10);
+
+            if (isNaN(count)) {
+                throw new Error('InvalidType');
+            }
+
+            result += char.repeat(count);
+        }
+
+        return result;
+    }
+
 }
 
 module.exports = compress
